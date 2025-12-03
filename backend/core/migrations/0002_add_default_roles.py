5c661deff2e9c6436fbad172cbf2bc2af5634c4d
Hila Miller <millerhila@gmail.com>
2025-12-03 19:36:55 +0200

    fix role migration

--- backend/core/migrations/0002_add_default_roles.py
+++ backend/core/migrations/0002_add_default_roles.py
@@ -4,26 +4,27 @@ def create_roles(apps, schema_editor):
     Role = apps.get_model('core', 'Role')
 
     roles = [
-        ("Admin", "System administrator"),
-        ("Trustee", "Community trustee"),
-        ("Borrower", "Loan borrower"),
-        ("Donor", "System donor"),
+        ("a3c4938e-03db-4364-a931-b8f942158a03", "Admin", "System administrator"),
+        ("4a38823a-11b8-419d-a91b-d3b8a162b132", "Trustee", "Community trustee"),
+        ("941eae3d-ca30-4d87-9474-5b55d5e8b219", "Borrower", "Loan borrower"),
+        ("c38fa941-0ed0-4c26-8770-135b1485f9c5", "Donor", "System donor"),
     ]
 
-    for name, desc in roles:
+    for role_id, name, desc in roles:
         Role.objects.update_or_create(
-            name=name,
-            defaults={"description": desc}
+            role_id=role_id,
+            defaults={"name": name, "description": desc}
         )
 
 def delete_roles(apps, schema_editor):
     Role = apps.get_model('core', 'Role')
     Role.objects.all().delete()
 
+
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '000X_create_roles'),
+        ('core', '0001_initial'),
     ]
 
     operations = [
